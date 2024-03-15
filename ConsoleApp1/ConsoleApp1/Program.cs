391ba2f62549321059d72ccd62a8605366b39615
Oliwier Szewko <s27540@pjwstk.edu.pl>
2024-03-15 09:37:09 +0100

    add Max method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+public class Max
+{
+
+    public static void Main(string[] args)
+    {
+
+        int[] arr = new[] {1,2,3,4,5,6,7,8};
+        Console.WriteLine(getMax(arr));
+
+    }
+
+    public static int getMax(int[] arr)
+    {
+        return arr.Max();
+    }
+    
+    
+}
