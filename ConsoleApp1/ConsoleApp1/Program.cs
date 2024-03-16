3bb8212b3fd24b3f2c1c233e6a971befde4c6223
s27540 <s27540@pjwstk.edu.pl>
2024-03-16 11:32:28 +0100

    dodanie writLine w forze

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public class Program
+public class Max
 {
+
     public static void Main(string[] args)
     {
+
+        int[] arr = new[] {1,2,3,4,5,6,7,8};
+        Console.WriteLine(getMax(arr));
+
         int[] bb = { 1, 2, 3, 4, 5 };
         Console.WriteLine(avg(bb));
     }
+
+    public static int getMax(int[] arr)
+    {
+        return arr.Max();
+    }
+  
     public static double avg(int[] arr)
     {
         double result = 0.0;
         for (int i = 0; i < arr.Length; i++)
         {
             result += arr[i];
+            Console.WriteLine(result);
         }
         return result/arr.Length;
     }
